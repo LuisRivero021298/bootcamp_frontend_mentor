30eaaab1e37566549c8e4a56b85a6f4139cc975c
LuisRivero021298 <luisda021298@gmail.com>
2020-07-11 17:16:05 -0500

    Using animate.css, js functions to change cards

--- static/index.js
+++ static/index.js
@@ -1,1 +1,26 @@
-console.log("Hello world");
+const tanyaArt = document.getElementById("tanya-art");
+const johnArt = document.getElementById("john-art");
+
+const changeToTanya = function (e) {
+  johnArt.style.display = "none";
+  tanyaArt.style.display = "grid";
+  if (e === "next") {
+    tanyaArt.classList.remove("animate__fadeInRight");
+    tanyaArt.classList.add("animate__fadeInLeft");
+  } else {
+    tanyaArt.classList.remove("animate__fadeInLeft");
+    tanyaArt.classList.add("animate__fadeInRight");
+  }
+};
+
+const changeToJohn = function (e) {
+  tanyaArt.style.display = "none";
+  johnArt.style.display = "grid";
+  if (e === "next") {
+    johnArt.classList.remove("animate__fadeInRight");
+    johnArt.classList.add("animate__fadeInLeft");
+  } else {
+    johnArt.classList.remove("animate__fadeInLeft");
+    johnArt.classList.add("animate__fadeInRight");
+  }
+};
